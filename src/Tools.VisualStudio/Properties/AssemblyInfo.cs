fa258a2533a07cd15521d7611511a0cb102ba89d
Nate McMaster <nate.mcmaster@microsoft.com>
2016-08-10 14:32:29 -0700

    Add missing, required assembly attributes

--- src/Tools.VisualStudio/Properties/AssemblyInfo.cs
+++ src/Tools.VisualStudio/Properties/AssemblyInfo.cs
@@ -6,6 +6,13 @@ using System.Resources;
 
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
+
+#if CSPROJ
+
+[assembly: AssemblyInformationalVersion("1.0.0-csproj")]
+
+#endif
+
 [assembly: AssemblyCompany("Microsoft Corporation.")]
 [assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
 [assembly: AssemblyProduct("Microsoft EntityFramework Core")]
